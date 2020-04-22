64c78b13ff5a421761a3f0d7bcc1703665d82a30
caueguedes <caue.guedes91@gmail.com>
2020-04-22 16:54:13 -0300

    chapter 05 - Sketching the behavior - 02

---
+++ book_content/05-unit-specs/02/spec/unit/app/api_spec.rb
@@ -0,0 +1,20 @@
+require_relative  '../../../app/api'
+
+module ExpenseTracer
+  RSpec.describe "API" do
+    describe 'POST /expense' do
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id'
+        it 'returns with a 200 (OK) '
+
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity)'
+
+      end
+
+    end
+  end
+end
